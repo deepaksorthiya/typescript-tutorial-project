5be3cac5e944e6f3de19881a709e89a642427e2c
Deepak Katariya <deepak.sorthiya@gmail.com>
2025-06-09 22:17:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+---
+
+### ** Typescript tutorial **
+
+---
+
+# Getting Started
+
+### Compile Typescript file
+
+```bash
+npx tsc index.ts --outDir out
+```
+
+### Run Generated javascript file
+
+```bash
+node out/index.js
+```
---
+++ index.ts
@@ -0,0 +1,18 @@
+class Person {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
+
+let person = new Person("Alice", 30);
+console.log(person);
